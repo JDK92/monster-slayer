320404fea85aa0ad8e8ca9127bec1978fd53d7e1
Jesús Alfredo Morales Damken <jesusdmk@gmail.com>
2019-07-25 23:05:40 -0600

    Added attack, special attack and healing. Now the lifebar color changes when health is below 50 and 20

--- app.js
+++ app.js
@@ -3,29 +3,109 @@ new Vue({
   data: {
     disabled: false,
     battleLog: [],
-    monsterHealth: {},
-    heroHealth: {},
-    lifebar: "",
+    hero: {},
+    monster: {},
+    heroLifebar: "Waiting",
+    monsterLifebar: "Waiting",
     heroDamage: "",
     monsterDamage: "",
+    heroHealing: "",
+    healthStatus: {},
     win: false,
     loss: false
   },
   methods: {
     startGame: function() {
-      this.lifebar = 100;
-      this.monsterHealth = {
-        width: this.lifebar + "%"
+      this.heroLifebar = 100;
+      this.monsterLifebar = 100;
+
+      this.hero = {
+        width: this.heroLifebar + "%"
       };
+
+      this.monster = {
+        width: this.monsterLifebar + "%"
+      };
+
+      this.battleLog.push("BEGIN!");
     },
     attack: function() {
-      this.heroDamage = Math.random() * 10;
-      this.lifebar = this.lifebar - this.heroDamage;
-      this.monsterHealth = {
-        width: this.lifebar + "%"
+      this.heroDamage = Math.round(Math.random() * 10);
+      this.monsterDamage = Math.round(Math.random() * 8);
+
+      this.monsterLifebar = this.monsterLifebar - this.heroDamage;
+      this.heroLifebar = this.heroLifebar - this.monsterDamage;
+
+      this.monster = {
+        width: this.monsterLifebar + "%"
       };
-      if (this.lifebar <= 0) {
-        this.win = true;
+
+      this.hero = {
+        width: this.heroLifebar + "%"
+      };
+
+      this.battleLog.push(
+        "ATTACK! Hero dealt " +
+          this.heroDamage +
+          " / " +
+          "Monster dealt " +
+          this.monsterDamage
+      );
+    },
+    specialAttack: function() {
+      this.heroDamage = Math.round(Math.random() * 12);
+      this.monsterDamage = Math.round(Math.random() * 10);
+
+      this.monsterLifebar = this.monsterLifebar - this.heroDamage;
+      this.heroLifebar = this.heroLifebar - this.monsterDamage;
+
+      this.monster = {
+        width: this.monsterLifebar + "%"
+      };
+
+      this.hero = {
+        width: this.heroLifebar + "%"
+      };
+
+      this.battleLog.push(
+        "SPECIAL ATTACK! Hero dealt " +
+          this.heroDamage +
+          " / " +
+          "Monster dealt " +
+          this.monsterDamage
+      );
+    },
+    heal: function() {
+      if (this.heroLifebar == 100) {
+        this.monsterDamage = Math.round(Math.random() * 10);
+
+        this.heroLifebar = this.heroLifebar - this.monsterDamage;
+
+        this.hero = {
+          width: this.heroLifebar + "%"
+        };
+
+        this.battleLog.push(
+          "YOU HAVE FULL HEALTH! Monster dealt " + this.monsterDamage
+        );
+      } else {
+        this.heroHealing = Math.round(Math.random() * 10);
+        this.monsterDamage = Math.round(Math.random() * 10);
+
+        this.heroLifebar =
+          this.heroLifebar + this.heroHealing - this.monsterDamage;
+
+        this.hero = {
+          width: this.heroLifebar + "%"
+        };
+
+        this.battleLog.push(
+          "HEALING! Hero healed " +
+            this.heroHealing +
+            " / " +
+            "Monster dealt " +
+            this.monsterDamage
+        );
       }
     }
   },
@@ -35,6 +115,45 @@ new Vue({
     },
     loss: function() {
       alert("Game Over");
+    },
+    monsterLifebar: function() {
+      if (this.monsterLifebar < 50) {
+        this.monsterStatus = {
+          backgroundColor: "yellow",
+          width: this.monsterLifebar + "%"
+        };
+      }
+
+      if (this.monsterLifebar < 20) {
+        this.monsterStatus = {
+          backgroundColor: "red",
+          width: this.monsterLifebar + "%"
+        };
+      }
+      if (this.monsterLifebar >= 50) {
+        this.monsterStatus = {
+          backgroundColor: "green"
+        };
+      }
+    },
+    heroLifebar: function() {
+      if (this.heroLifebar < 50) {
+        this.heroStatus = {
+          backgroundColor: "yellow",
+          width: this.heroLifebar + "%"
+        };
+      }
+      if (this.heroLifebar < 20) {
+        this.heroStatus = {
+          backgroundColor: "red",
+          width: this.heroLifebar + "%"
+        };
+      }
+      if (this.heroLifebar >= 50) {
+        this.heroStatus = {
+          backgroundColor: "green"
+        };
+      }
     }
   }
 });
